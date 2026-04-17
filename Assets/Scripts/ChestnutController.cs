d1a188b95b15e9adf94f859bcd63b8b4a6033d2a
All298Lie <74299604+All298Lie@users.noreply.github.com>
2026-04-17 16:20:36 +0900

    6주차 실습

---
+++ Assets/Scripts/ChestnutController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ChestnutController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private ParticleSystem particleSystem;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        Shoot(new Vector3(0.0f, 200.0f, 2000.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        rigidbody.isKinematic = true;
+
+        if (other.gameObject.CompareTag("Target") == true)
+        {
+            particleSystem.Play();
+        }
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        rigidbody.AddForce(dir);
+    }
+}
---
+++ Assets/Scripts/ChestnutGenerator.cs
@@ -0,0 +1,82 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Pool;
+
+public class ChestnutGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject chestnutPrefab;
+    [SerializeField] private InputAction leftclickAction;
+    [SerializeField] private InputAction pointer;
+
+    private ObjectPool<GameObject> pool;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        pool = new ObjectPool<GameObject>(CreateChestnut, GetChestnut, ReleaseChestnut);
+    }
+
+    void OnEnable()
+    {
+        leftclickAction.Enable();
+        pointer.Enable();
+
+        leftclickAction.performed += HandleClick;
+    }
+
+    void OnDisable()
+    {
+        leftclickAction.performed -= HandleClick;
+
+        leftclickAction.Disable();
+        pointer.Disable();
+    }
+
+    private void HandleClick(InputAction.CallbackContext context)
+    {
+        Vector2 screenPos = this.pointer.ReadValue<Vector2>();
+
+        GameObject chestnut = pool.Get();
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+        chestnut.GetComponent<ChestnutController>().Shoot(ray.direction * 2000);
+
+        RemoveChestnut(chestnut).Forget();
+    }
+
+    private async UniTaskVoid RemoveChestnut(GameObject chestnut)
+    {
+        await UniTask.Delay(1500);
+
+        pool.Release(chestnut);
+    }
+
+    private GameObject CreateChestnut()
+    {
+        GameObject chestnut = Instantiate(chestnutPrefab, new Vector3(0.0f, 1.0f, -9.0f), Quaternion.identity, transform);
+        chestnut.name = "Chesetnut";
+
+        return chestnut;
+    }
+
+    private void GetChestnut(GameObject chestnut)
+    {
+        chestnut.SetActive(true);
+
+        chestnut.transform.position = new Vector3(0.0f, 1.0f, -9.0f);
+        if (chestnut.TryGetComponent<Rigidbody>(out Rigidbody rb) == true)
+        {
+            rb.isKinematic = false;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void ReleaseChestnut(GameObject chestnut)
+    {
+        chestnut.SetActive(false);
+    }
+}
